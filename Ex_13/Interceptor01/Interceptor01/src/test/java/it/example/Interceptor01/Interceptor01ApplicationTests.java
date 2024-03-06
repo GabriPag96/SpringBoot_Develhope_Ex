cd5917df1b6fd4d205de6372f913097ad57efd61
Gabri <gabripag@outlook.com>
2024-03-06 15:31:20 +0100

    UF Ex_12

---
+++ Ex_13/Interceptor01/Interceptor01/src/test/java/it/example/Interceptor01/Interceptor01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.example.Interceptor01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interceptor01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
