0e1f36e251eb4f1eeb9935aee304510fcd4d5f4d
Gabri <gabripag@outlook.com>
2024-03-14 19:00:02 +0100

    ADDED Ex_19

---
+++ Ex_19/deployExerciseEnvironmentOne/deployExerciseEnvironmentOne/src/test/java/it/example/deployExerciseEnvironmentOne/DeployExerciseEnvironmentOneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.example.deployExerciseEnvironmentOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployExerciseEnvironmentOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
