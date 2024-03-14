0deb6a6885ee43ca3a3bd402f4cec8c369c880a5
Gabri <gabripag@outlook.com>
2024-03-14 18:06:42 +0100

    UF

---
+++ Ex_18/deployExerciseTwo/deployExerciseTwo/src/main/java/it/example/deployExerciseTwo/DeployExerciseTwoApplication.java
@@ -0,0 +1,13 @@
+package it.example.deployExerciseTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployExerciseTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployExerciseTwoApplication.class, args);
+	}
+
+}
---
+++ Ex_18/deployExerciseTwo/deployExerciseTwo/src/test/java/it/example/deployExerciseTwo/DeployExerciseTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.example.deployExerciseTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployExerciseTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
