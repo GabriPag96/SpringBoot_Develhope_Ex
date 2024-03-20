6341ea6ccc64820310f47b7474c392488c8c2303
Gabri <gabripag@outlook.com>
2024-03-20 15:36:32 +0100

    Ex.15-16 MODDED

---
+++ Ex_15+16/CustomQueryOneandTwo/CustomQueryOneandTwo/src/main/java/it/example/CustomQueryOneandTwo/CustomQueryOneandTwoApplication.java
@@ -0,0 +1,13 @@
+package it.example.CustomQueryOneandTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomQueryOneandTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomQueryOneandTwoApplication.class, args);
+	}
+
+}
---
+++ Ex_15+16/CustomQueryOneandTwo/CustomQueryOneandTwo/src/test/java/it/example/CustomQueryOneandTwo/CustomQueryOneandTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.example.CustomQueryOneandTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomQueryOneandTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
